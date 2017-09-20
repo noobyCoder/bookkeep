21ebb79021d8f2abe7410847383128e4ee3af920
xieyao <xieyao>
2017-09-20 09:53:27 +0800

    add MainActivity

---
+++ app/src/main/java/bookkeep/michael/com/bookkeeping/MainActivity.kt
@@ -0,0 +1,31 @@
+package bookkeep.michael.com.bookkeeping
+
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import bookkeep.michael.com.bookkeeping.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
---
+++ app/src/main/java/bookkeep/michael/com/bookkeeping/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package bookkeep.michael.com.bookkeeping.viewmodel
+
+/**
+ * Created by xieyao on 2017/9/19.
+ */
+class MainViewModel : ViewModel {
+}
